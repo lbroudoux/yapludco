34153edd68ef802baf527ae356879e5be7a648b9
Laurent Broudoux <laurent.broudoux@gmail.com>
2019-08-04 21:33:36 +0200

    Adding root module

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { StorageServiceModule } from 'ngx-webstorage-service';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AuthenticationHttpInterceptor } from './services/auth.http-interceptor';
+import { AuthenticationServiceProvider } from './services/auth.service.provider';
+import { LocalStorageService } from './services/local-storage.service';
+import { DashboardPageComponent } from './pages/dashboard/dashboard.page';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    StorageServiceModule
+  ],
+  declarations: [
+    AppComponent, DashboardPageComponent
+  ],
+  providers: [
+    LocalStorageService,
+    AuthenticationServiceProvider, 
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthenticationHttpInterceptor,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
